50ea5cc349fef5af5f95599978f258a411024ad6
Arthur Zopellaro <arthurazsoares@gmail.com>
2019-10-15 13:07:02 -0300

    Improve user experience

--- mememu.py
+++ mememu.py
@@ -1,17 +1,27 @@
+from sys import argv
 from mapping import Cache
-from mapping import DIRECT, ASSOCIATIVE, SET_ASSOCIATIVE
-from mapping import RANDOM, FIFO, LRU, LFU
+from mapping import DIRECT, SET_ASSOCIATIVE
 
 
-FILENAME = 'example.txt'
-CACHE_SIZE = 4
-MAPPING = ASSOCIATIVE
-POLICY = FIFO
-FRAME_SIZE = 2
+policy = None
+frame_size = None
+with open(argv[1]) as addresses:
+    cache_size = int(input('Entre com o tamanho da cache: '))
 
-with open(FILENAME) as addresses:
-    cache = Cache(mapping=MAPPING, cache_size=CACHE_SIZE,
-                  policy=POLICY, frame_size=FRAME_SIZE)
+    print('\nMapeamento:')
+    print(' 1 - Direto\n 2 - Associativo\n 3 - Associativo por Conjunto')
+    mapping = int(input('Escolha o número do mapeamento desejado: '))
+    if mapping != DIRECT:
+        print('\nPolítica de Substituição:')
+        print(' 1 - Aleatório')
+        print(' 2 - FIFO (First in First Out)')
+        print(' 3 - LRU (Least Recently Used)')
+        print(' 4 - LFU (Least Frequently Used)')
+        policy = int(input('Escolha o número da política desejada: '))
+    if mapping == SET_ASSOCIATIVE:
+        frame_size = int(input('\nEntre com o tamanho do conjunto: '))
+    cache = Cache(mapping=mapping, cache_size=cache_size,
+                  policy=policy, frame_size=frame_size)
     for tag in addresses.readlines():
         tag = int(tag.strip())
         cache.alloc(tag)
